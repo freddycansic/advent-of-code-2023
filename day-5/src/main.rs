2c97fbf97d622e599fcbdfa93f5c4b7df3f032a5
Freddy Cansick <freddycansick@gmail.com>
2023-12-05 15:44:40 +0000

    Day 5 formatted

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, ops::Range};
 
 fn main() {
     let input = include_str!("input.txt").trim();
@@ -7,24 +7,60 @@ fn main() {
     println!("Part 2 = {}", part_2(input));
 }
 
-fn part_1(input: &str) -> u64 {
+struct Mapping {
+    source: Range<u64>,
+    destination_start: u64,
+}
+
+fn create_mappings(sections: &[Vec<u64>]) -> Vec<Vec<Mapping>> {
+    sections
+        .iter()
+        .map(|section| {
+            let mut map = Vec::<Mapping>::new();
+
+            for mut chunk in &section.into_iter().chunks(3) {
+                let destination_start = chunk.next().unwrap();
+                let source_start = chunk.next().unwrap();
+                let length = chunk.next().unwrap();
+
+                map.push(Mapping {
+                    source: *source_start..*source_start + *length,
+                    destination_start: *destination_start,
+                });
+            }
+
+            map
+        })
+        .collect::<Vec<Vec<Mapping>>>()
+}
+
+fn apply_mappings(seed: u64, mappings: &[Vec<Mapping>]) -> u64 {
+    let mut current = seed;
+
+    for map in mappings.iter() {
+        for mapping in map.iter() {
+            if mapping.source.contains(&current) {
+                current = mapping.destination_start + current.abs_diff(mapping.source.start);
+                break;
+            }
+        }
+    }
+
+    current
+}
+
+fn get_seeds_and_sections(input: &str) -> (Vec<u64>, Vec<Vec<u64>>) {
     let mut sections = input
-        .split("\r\n\r")
-        // .split("\n\n")
+        .split("\n\n")
         .map(str::trim)
         .map(|section| {
-            // dbg!(section);
-
             section
                 .split_once(":")
                 .unwrap()
                 .1
                 .trim()
                 .split_whitespace()
-                .map(|num| {
-                    // dbg!(num);
-                    num.parse::<u64>()
-                })
+                .map(str::parse)
                 .map(Result::unwrap)
                 .collect()
         })
@@ -32,136 +68,36 @@ fn part_1(input: &str) -> u64 {
 
     let seeds = sections.pop_front().unwrap();
 
-    let mappings = sections
+    (seeds, sections.into())
+}
+
+fn part_1(input: &str) -> u64 {
+    let (seeds, sections) = get_seeds_and_sections(input);
+
+    let mappings = create_mappings(&sections);
+
+    seeds
         .into_iter()
-        .map(|section| {
-            let mut map = Vec::<(std::ops::Range<u64>, std::ops::Range<u64>)>::new();
-
-            for mut chunk in &section.into_iter().chunks(3) {
-                let dest_range_start = chunk.next().unwrap();
-                let source_range_start = chunk.next().unwrap();
-                let range_length = chunk.next().unwrap();
-
-                map.push((
-                    source_range_start..source_range_start + range_length,
-                    dest_range_start..dest_range_start + range_length,
-                ));
-            }
-
-            map
-        })
-        .collect::<Vec<Vec<(std::ops::Range<u64>, std::ops::Range<u64>)>>>();
-
-    let min_location = seeds
-        .into_iter()
-        .map(|seed| {
-            let mut current = seed;
-
-            for (index, map) in mappings.iter().enumerate() {
-                dbg!(index);
-                for (source, dest) in map.iter() {
-                    if source.contains(&current) {
-                        dbg!(current);
-
-                        current = dest.clone().nth(0).unwrap()
-                            + current.abs_diff(source.clone().nth(0).unwrap());
-                        dbg!(current);
-                        break;
-                    }
-                }
-            }
-
-            current
-        })
+        .map(|seed| apply_mappings(seed, &mappings))
         .min()
-        .unwrap();
-
-    min_location
+        .unwrap()
 }
 
 fn part_2(input: &str) -> u64 {
-    let mut sections = input
-        .split("\r\n\r")
-        // .split("\n\n")
-        .map(str::trim)
-        .map(|section| {
-            dbg!(section);
+    let (seeds, sections) = get_seeds_and_sections(input);
 
-            section
-                .split_once(":")
-                .unwrap()
-                .1
-                .trim()
-                .split_whitespace()
-                .map(|num| num.parse::<u64>())
-                .map(Result::unwrap)
-                .collect()
-        })
-        .collect::<VecDeque<Vec<u64>>>();
+    let seed_ranges = seeds.chunks(2).map(|chunk| chunk[0]..chunk[0] + chunk[1]);
 
-    let seeds = sections.pop_front().unwrap();
-
-    // let seeds = seeds
-    //     .iter()
-    //     .chunks(2)
-    //     .map(|chunk| (*start..start + length).collect::<Vec<u64>>())
-    //     .flatten();
-    let mut seeds2 = Vec::new();
-    for i in (0..seeds.len()).step_by(2) {
-        let start = seeds[i];
-        let range = seeds[i + 1];
-
-        seeds2.push(start..start + range);
-    }
-
-    let mappings = sections
-        .into_iter()
-        .map(|section| {
-            let mut map = Vec::<(std::ops::Range<u64>, std::ops::Range<u64>)>::new();
-
-            for mut chunk in &section.into_iter().chunks(3) {
-                let dest_range_start = chunk.next().unwrap();
-                let source_range_start = chunk.next().unwrap();
-                let range_length = chunk.next().unwrap();
-
-                map.push((
-                    source_range_start..source_range_start + range_length,
-                    dest_range_start..dest_range_start + range_length,
-                ));
-            }
-
-            map
-        })
-        .collect::<Vec<Vec<(std::ops::Range<u64>, std::ops::Range<u64>)>>>();
+    let mappings = create_mappings(&sections);
 
     let mut min_location = u64::MAX;
 
-    for seed_range in seeds2 {
+    for seed_range in seed_ranges {
         for seed in seed_range {
-            let mut current = seed;
+            let location = apply_mappings(seed, &mappings);
 
-            if current % 1_000_000 == 0 {
-                println!("{current}");
-            }
-
-            for (index, map) in mappings.iter().enumerate() {
-                // dbg!(index);
-                for (source, dest) in map.iter() {
-                    if source.contains(&current) {
-                        // dbg!(current);
-
-                        current = dest.clone().nth(0).unwrap()
-                            + current.abs_diff(source.clone().nth(0).unwrap());
-                        // dbg!(current);
-                        break;
-                    }
-                }
-            }
-
-            min_location = min_location.min(current);
+            min_location = min_location.min(location);
         }
-
-        println!("New seed range");
     }
 
     min_location
